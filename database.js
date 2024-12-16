8b849c5ae9f890e6fa4ac30dbe862d32669cd206
Artres102 <150268118+Artres102@users.noreply.github.com>
2024-12-16 16:56:49 +0000

    Update database.js

--- database.js
+++ database.js
@@ -5,12 +5,14 @@ var hostname = process.env.DB_HOST;
 var database = process.env.DB_NAME;
 var username = process.env.DB_USER;
 var password = process.env.DB_PASSWORD;
+var port = process.env.DB_PORT
 
 var connection = mysql.createConnection({
   host: hostname,
   user: username,
   password: password,
   database: database,
+  port: port
 });
 
 module.exports = connection
