f0dabb438a234ce7c99adfb0f1655ea085fb282f
Artres102 <150268118+Artres102@users.noreply.github.com>
2024-12-17 15:16:26 +0000

    Update rooms.js

--- API/rooms.js
+++ API/rooms.js
@@ -15,7 +15,7 @@ router.get("/getCurrentRoom/:sessionId", (req, res) => {
                 return err;
             };
             roomId = results[0].gamesession_current_room_id;
-            
+            console.log("CONAS")
             res.json(roomId);
         }
     );
